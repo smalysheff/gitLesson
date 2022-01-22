4ec6c4d14edd2fd6df523a5a41966375b831fd24
Malyshev Sergey <malshevs_v@mail.ru>
2022-01-22 13:02:24 +0000

    add Experimental.java

---
+++ Experimental.java
@@ -0,0 +1,17 @@
+public class Experimental {
+	private int id;
+	private String exp;
+
+	public Experimental(int id, String exp) {
+		this.id = id;
+		this.exp = exp;
+	}
+	
+	@Override
+	public String toString() {
+		return "Experimental (" + 
+			"id: " + id + 
+			"exp: " + exp
+			")";
+	}
+}
